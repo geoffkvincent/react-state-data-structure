59277c0bcf516d0e01c0903841dde6fade224eda
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-07 14:44:23 -0600

    map over app state

---
+++ src/InitialData.js
@@ -0,0 +1,11 @@
+const initialData = {
+  todos: [
+    {id: 1, content: 'Learn JS', complete: true},
+    {id: 2, content: 'Learn React', complete: false},
+    {id: 3, content: 'Learn Redux', complete: false},
+    {id: 4, content: 'Get a Job', complete: false},
+    {id: 5, content: 'Make a Million Bucks', complete: false},
+  ]
+}
+
+export default initialData
