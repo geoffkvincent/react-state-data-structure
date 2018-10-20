fb87bdc1eec90a22f31851f3fcfabeb93618c31e
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-20 13:57:14 -0600

    add item func

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import Form from './Form'
 class App extends React.Component {
   state = {todos: [] }
 
-  addItem = () => {
-  
+  getUniqId = () => {
+    return Math.floor(( 1 + Math.random()) * 0x10000)
+    .toString(16)
+    .subString(1)
+  }
+
+  addItem = (item) => {
+    const { todos } = this.state
+    const todo = { item, id: this.getUniqId(), complete: false}
+    this.setState({todos:[todo, ...todos] })
   }
   
   render() {
