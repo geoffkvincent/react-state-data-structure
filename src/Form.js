92674e754d432ad86ac0a7ae9f94fea9255f3f44
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-20 13:16:21 -0600

    handle change

--- src/Form.js
+++ src/Form.js
@@ -2,12 +2,18 @@ import React from 'react'
 
 class Form extends React.Component {
   state = { item: [] }
+
+  handleSubmit = () => {
+    preventDefault()
+  }
+
   render() {
     return (
       <form>
         <input
           required
           placeholder= 'Add Item'
+          onChange
         />
       </form>
     )
