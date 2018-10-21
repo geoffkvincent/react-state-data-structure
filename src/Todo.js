d27ee44a36c7facbd762693952f4b157c7e6a6a5
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-21 08:51:17 -0600

    todo click

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
-const Todo = () => {
- 
+const styles = {
+  todo: {cursor: 'pointer'},
+  complete: {color: 'grey', textDecoration: 'line-through'}
+}
+
+const Todo = ({id, item, complete, todoClick}) => {
+  <li
+    style={complete ? {...styles.todo, ...styles.complete } : styles.todo }
+    onClick={ () => todoClick(id) }
+  >
+    {item}
+  </li>
 }
 
 export default Todo
