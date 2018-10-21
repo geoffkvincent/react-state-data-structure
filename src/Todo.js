c0740e419e2c161cf890ee38ff5549e248d39f99
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-21 09:14:57 -0600

    syntax fix

--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const styles = {
-  todo: {cursor: 'pointer'},
-  complete: {color: 'grey', textDecoration: 'line-through'}
+  todo: { cursor: 'pointer' },
+  complete: { color: 'grey', textDecoration: 'line-through' }
 }
 
-const Todo = ({id, item, complete, todoClick}) => {
+const Todo = ({ id, item, complete, todoClick }) => (
   <li
     style={ complete ? { ...styles.todo, ...styles.complete } : styles.todo }
     onClick={ () => todoClick(id) }
-  >     
-    {item}
+  >
+   {item}
   </li>
-}
+)
 
-export default Todo
+export default Todo;
