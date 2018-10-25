737d52c1db7768371e814b3856df9af9e80d168c
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-25 08:51:49 -0600

    todos destructure

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ class App extends React.Component {
   state = initialData
 
   render() {
+    const {todos} = this.state
     return(
       <div>
-        <List />
+        <List todos={todos}/>
       </div>
     )
   }
