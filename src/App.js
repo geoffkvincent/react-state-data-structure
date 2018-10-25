dcd3d1d011e447641056d772625b403f027b291c
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-25 09:27:34 -0600

    add item function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react'
-
 import List from './List'
 import Form from './Form'
 
 class App extends React.Component {
-  state = 
+  state = { todos: [] }
+
+  getId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+  }
+
+  addItem = (name) => {
+    const {todos} = this.state
+    const todo = ({name, id: this.getId(), complete:false})
+    this.setState([todo, ...todos])
+  }
 
   render() {
     const {todos} = this.state
     return(
       <div>
-        <Form />
+        <Form addItem={this.addItem}/>
         <List todos={todos} />
       </div>
     )
