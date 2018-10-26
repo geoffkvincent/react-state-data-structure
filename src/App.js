56dd144a9ac4cb6b830ecd8c4f7115b0d9631c0b
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-26 08:36:31 -0600

    initial start

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
 import React from 'react'
-import List from './List'
-import Form from './Form'
 
 class App extends React.Component {
-  state = { todos: [] }
 
-  getId = () => {
-    return Math.floor((1 + Math.random()) * 0x10000)
-  }
-
-  addItem = (name) => {
-    const {todos} = this.state
-    const todo = ({name, id: this.getId(), complete:false})
-    this.setState({ todos: [todo, ...todos] })
-  }
-
-  render() {
-    const {todos} = this.state
-    return(
-      <div>
-        <Form addItem={this.addItem}/>
-        <List todos={todos} />
-      </div>
-    )
-  }
 }
 
 export default App
