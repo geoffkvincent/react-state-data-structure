c272c80534502526303d590d32e6c5218b9c97b0
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-26 08:39:28 -0600

    render null

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import InitialState from './InitialState'
 
 class App extends React.Component {
   state = InitialState
+  
+  render() {
+    return (
+      null
+    )
+  }
 }
 
 export default App
