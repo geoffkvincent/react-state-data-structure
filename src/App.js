c3cd4f449dcdc48cb9d76e27690206c2f65f6d05
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-26 08:48:12 -0600

    seed bug fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import InitialState from './InitialState'
+import initialState from './InitialState'
 import List from './List'
 
 class App extends React.Component {
-  state = InitialState
+  state = initialState
   
   render() {
     const {todos} = this.state
