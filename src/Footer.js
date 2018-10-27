88332bd1fd57d9fba57ac0c9438655390e465851
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-27 13:24:19 -0600

    filterlink

--- src/Footer.js
+++ src/Footer.js
@@ -9,3 +9,10 @@ const styles = {
     marginRight: '5px',
   }
 }
+
+const filterLink = (current, name, setFilter) => {
+  if (current === name)
+    return <span>{name}</span>
+  else
+    return <span style={styles.link} onClilck={() => setFilter(name) }>{name}</span>
+}
