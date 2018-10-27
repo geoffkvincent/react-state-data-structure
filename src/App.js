887d684967a4155a168da8f855216e9ebbc085d7
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-27 13:10:39 -0600

    handle click

--- src/App.js
+++ src/App.js
@@ -14,6 +14,21 @@ class App extends React.Component {
     const todo = {name, id: this.getId(), complete: false}
     this.setState({todos : [todo, ...todos]})
   }
+
+  handleClick = (id) => {
+    const {todos} = this.state
+    this.setState({
+      todos: todos.map(todo => {
+        if(todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete
+          }
+        }
+        return todo
+      })
+    })
+  }
   
   render() {
     const {todos} = this.state
