dd30f4e063f7054ad0e854370d7e5cf3747950a4
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-28 09:06:09 -0600

    list setup

--- src/List.js
+++ src/List.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const List = () => {
-
+const List = ({todos}) => {
+  <div>
+    {todos.map(t =>
+      <li key={t.id}>{t.content}</li>  
+    )} 
+  </div>
 }
 
 export default List
