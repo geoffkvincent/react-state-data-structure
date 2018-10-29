7329047cfe2c74e8f73a0f976aace588135339a4
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-29 14:10:26 -0600

    handle change

--- src/Form.js
+++ src/Form.js
@@ -3,9 +3,21 @@ import React from 'react'
 class Form extends React.Component {
   state={ name: '' }
 
+  handleChange = (e) => {
+    this.setState({name: e.target.value})
+  }
+
   render() {
+    const {name} = this.state
     return (
-      null
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          value={name}
+          onChange={this.handleChange}
+          required
+          placeholder="add item"
+        />
+      </form>
     )
   }
 }
