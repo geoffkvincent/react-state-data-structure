4ae3df6e9c583f52b004cd298724042cd9765a51
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-29 10:11:36 -0600

    app setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import initialData from './InitialData'
+
+class App extends React.Component {
+  state= initialData
+
+  render() {
+    return(
+      null
+    )
+  }
+}
+
+export default App
