9c75b85072982f82f2383fc003a40a9140a2d2e8
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-22 10:19:03 -0700

    passing todos as props

--- src/List.js
+++ src/List.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const List = ({todos}) => (
   <div>
-    {todos.map(todo => 
-      <li>{todo.content}</li>
-    )}
+    { todos.map(todo => 
+        <li key={todo.id}>
+          {todo.content}
+        </li>
+      )
+    }
   </div>        
 )
 
