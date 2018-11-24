f2ff88e33ca837e814bcf0b26f6d240b9ab28c8e
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-24 15:09:39 -0700

    todo form

--- src/Form.js
+++ src/Form.js
@@ -3,12 +3,22 @@ import React from 'react'
 class Form extends React.Component {
   state = { name: ''}
 
+  handleSubmit = () => {
+
+  }
+
+  handleChange = () => {
+
+  }
+
   render() {
     const {name} = this.state
     return(
-      <form>
+      <form onSubmit = {this.handleSubmit}>
         <input 
           value = {name}
+          name = 'name'
+          onChange = {this.handleChange}
           required
           placeholder = 'Item'
         />
